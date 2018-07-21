da57cee25387d750c4c277b9f18718ed7f3e7ca5
Anton Sadovnikov <anton.sadovnikov@rambler-co.ru>
2018-07-22 01:08:10 +0300

    added symfony/profiler-pack

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
