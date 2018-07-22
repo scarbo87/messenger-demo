82a08e9e684e346a54c8c6d0b1da17644074482a
Anton Sadovnikov <anton.sadovnikov@rambler-co.ru>
2018-07-22 21:14:02 +0300

    added symfony/monolog-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
